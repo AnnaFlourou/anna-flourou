13a43063ab0a59051d916a41acc935a446e648d5
Anna Flourou <af@wecom4u.com>
2019-12-04 17:35:44 +0100

    integration design for construction page

---
+++ components/common/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="nav-bar w-100 d-flex ai-center jc-center">
+    <h1>Anna FLOUROU</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
