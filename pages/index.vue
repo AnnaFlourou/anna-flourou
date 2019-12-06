b37fb6d3f4aa5da9fd5a17b84feb016ace499013
Anna Flourou <af@wecom4u.com>
2019-12-06 11:44:34 +0100

    set up only nuxt project

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,19 @@
 <template>
-  <div class="homepage container">
-    <div class="row ai-center jc-center content">
-       <h1 class="pt20">Site en construction</h1>
-      <div class="img-shape">
-        <img src="@/assets/media/img/woman-coding-3.png" alt="img home" />
-      </div>
-      
+  <section class="container">
+    <div>
+
     </div>
-    <!-- <video autoplay muted loop id="myVideo">
-      <source src="@/assets/media/video/construction.mp4" type="video/mp4" />
-    </video>
-    <div class="content w-100 d-flex jc-center">
-      <h1 class="m20">Site en construction</h1>
-    </div>-->
-  </div>
+  </section>
 </template>
 
 <script>
+
 export default {
-  components: {}
-};
+  components: {
+  }
+}
 </script>
+
+<style>
+</style>
 
